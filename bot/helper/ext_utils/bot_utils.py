02c6a4047437117993404bff8358452d911b000b
ramxsh <91254304+ramxsh@users.noreply.github.com>
2022-04-06 15:36:30 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -152,14 +152,14 @@ def get_readable_message():
                 except:
                     pass
                 msg += f'\n<b>👨🏽‍💻 By:</b> ️<code>{download.message.from_user.first_name}</code>️(<code>{download.message.from_user.id}</code>)'
-                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n<code> /{BotCommands.CancelMirror} {download.gid()}</code>"
             elif download.status() == MirrorStatus.STATUS_SEEDING:
                 msg += f"\n<b>🗂 Size: </b>{download.size()}"
                 msg += f"\n<b>⚡️ Speed: </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
                 msg += f" | <b>📤 Uploaded: </b>{get_readable_file_size(download.torrent_info().uploaded)}"
                 msg += f"\n<b>Ratio: </b>{round(download.torrent_info().ratio, 3)}"
                 msg += f" | <b>Time: </b>{get_readable_time(download.torrent_info().seeding_time)}"
-                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n<code> /{BotCommands.CancelMirror} {download.gid()}</code>"
             else:
                 msg += f"\n<b>🗂 Size: </b>{download.size()}"
             msg += "\n\n"
